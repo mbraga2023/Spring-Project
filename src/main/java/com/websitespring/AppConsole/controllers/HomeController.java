af412df596b80eb8df42a3b6e365debdd0d8ba8b
Michel <michel.diener@gmail.com>
2023-08-07 08:56:03 -0300

    update login and cookie code

--- src/main/java/com/websitespring/AppConsole/controllers/HomeController.java
+++ src/main/java/com/websitespring/AppConsole/controllers/HomeController.java
@@ -1,15 +1,21 @@
 package com.websitespring.AppConsole.controllers;
 
+import java.io.UnsupportedEncodingException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.websitespring.AppConsole.Servico.CookieService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index(Model model){
+  public String index(Model model, HttpServletRequest request) throws UnsupportedEncodingException{
 
-        model.addAttribute("nome", "Michel"); //set attibute in main page
+        model.addAttribute("nome", CookieService.getCookie(request, "user_name")); //set attibute in main page
         
         return "/home/index";
     }
--- src/main/java/com/websitespring/AppConsole/controllers/LoginController.java
+++ src/main/java/com/websitespring/AppConsole/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package com.websitespring.AppConsole.controllers;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.websitespring.AppConsole.Servico.CookieService;
 import com.websitespring.AppConsole.models.Administrador;
 import com.websitespring.AppConsole.repositorio.AdministradoresRepo;
 
@@ -34,27 +37,43 @@ public class LoginController {
         Administrador administrador = repo.findByEmailAndSenha(email, senha);
         if (administrador != null) {
             // Successful login, you can set up the user session or authentication here
-            Cookie emailCookie2 = new Cookie("user_email", email);
-            emailCookie2.setMaxAge(60 * 60); // 1 hour in seconds
-            response.addCookie(emailCookie2);
-            // Check if "lembrar" checkbox is checked
+            // Get the name from the Administrador object
+            String name = administrador.getNome();
+
+            Cookie emailCookie = new Cookie("user_email", email);
+            emailCookie.setMaxAge(60 * 60); // 1 hour in seconds
+            response.addCookie(emailCookie);
+
+            Cookie nameCookie = new Cookie("user_name", name);
+            nameCookie.setMaxAge(60 * 60); // 1 hour in seconds
+            response.addCookie(nameCookie);
+
+            //Check if "lembrar" checkbox is checked
+
             if (lembrar != null && lembrar.equals("on")) {
                 // Create a cookie for the email and set it to expire in 30 days (adjust the max
                 // age as needed)
-                Cookie emailCookie = new Cookie("user_email", email);
-                emailCookie.setMaxAge(30 * 24 * 60 * 60); // 30 days in seconds
-                response.addCookie(emailCookie);
+                Cookie emailCookieLong = new Cookie("user_email", email);
+                emailCookieLong.setMaxAge(30 * 24 * 60 * 60); // 30 days in seconds
+                response.addCookie(emailCookieLong);
 
                 // You can create another cookie for the "lembrar" checkbox if needed
                 // Cookie lembrarCookie = new Cookie("lembrar", "true");
                 // lembrarCookie.setMaxAge(30 * 24 * 60 * 60);
                 // response.addCookie(lembrarCookie);
-            } 
+            }
 
             return "redirect:/"; // Redirect to the home page after successful login
         } else {
             model.addAttribute("erro", "Usuário ou senha inválidos");
             return "login/index"; // Return to the login form with an error message
         }
+    }
+
+    @GetMapping("/sair")
+    public String logout(HttpServletResponse response) throws IOException {
+        CookieService.setCookie(response, "user_email", "", 0);
+                CookieService.setCookie(response, "user_name", "", 0);
+        return "redirect:login/index";
     }
 }
